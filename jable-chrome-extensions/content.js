55d5289cebbf024b84a2fee831fb109134d97c59
Halo <lindaoling@163.com>
2025-07-15 23:49:40 +0800

    升级到manifest version 3

--- jable-chrome-extensions/content.js
+++ jable-chrome-extensions/content.js
@@ -1,21 +1,28 @@
-﻿// 向页面注入script，为了得到页面中js变量
+﻿// 向页面注入script
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('inject.js');
-s.onload = function() { this.remove(); };
+// indect.js加载完后，向inject.js发送配置信息
+s.onload = function() { 
+  this.remove(); 
+  // console.log('通过postMessage传递配置参数到inject.js')
+  // 通过postMessage传递配置参数到inject.js
+  chrome.storage.local.get().then((settingsObj)=> {
+    // console.log('chrome.storage.local.get() settingsObj:',settingsObj)
+      settingsObj.workDir = settingsObj.workDir || '';
+      window.postMessage({
+          type: 'JABLE_SETTINGS',
+          settings: settingsObj
+      }, '*');
+  }).catch(error =>{
+      console.error('获取存储设置失败:', error);
+      // 依然postMessage一个空配置
+      window.postMessage({
+          type: 'JABLE_SETTINGS',
+          settings: { workDir: '',msg:'这是默认配置处理' }
+      }, '*');
+  });
+};
 (document.head || document.documentElement).appendChild(s);
 
-// 通过postMessage传递配置参数到inject.js
-chrome.storage.local.get().then(function(settingsObj) {
-    settingsObj.workDir = settingsObj.workDir || '';
-    window.postMessage({
-        type: 'JABLE_SETTINGS',
-        settings: settingsObj
-    }, '*');
-}).catch(function(error) {
-    console.error('获取存储设置失败:', error);
-    // 依然postMessage一个空配置
-    window.postMessage({
-        type: 'JABLE_SETTINGS',
-        settings: { workDir: '' }
-    }, '*');
-});
+
+
